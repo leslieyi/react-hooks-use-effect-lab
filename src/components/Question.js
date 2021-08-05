3283cd92ed75f6610bf90e72f9e65ebbb243983e
leslieyi <leslieyi411@gmail.com>
2021-08-04 21:10:24 -0400

    I still don't get this

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    //timeRemaining is 0? then setTimeremaining(10)
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+      return;
+    }
+    const timerID = setTimeout(() => setTimeRemaining((timeRemaining)=>timeRemaining-1), 1000);
+    return function cleanup () {
+      clearTimeout(timerID)
+    }
+  },[timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
